65b8cc156fb0e6e3222fc933742961b88530e05d
Alexander Pope <alex@pope-industries.com>
2013-03-14 12:08:34 +0100

    refactor package resolution; added source resolution; added css exceptions

--- index.js
+++ index.js
@@ -1,57 +1,128 @@
 var path = require('path')
 	, fs = require('fs')
 	, existsSync = fs.existsSync || path.existsSync
-	, DEFAULT_TYPES = ['js', 'css'];
+	, DEFAULT = {
+		js: {
+			extensions: ['js'],
+			sources: ['node_modules']
+		},
+		css: {
+			extensions: ['css'],
+			sources: []
+		}
+	};
 
 /**
+ * Resolve the path for 'dependency' from 'filepath'
+ * @param {String} filepath
+ * @param {String} dependency
+ * @param {Object} options
  * @returns {String}
  */
 module.exports = function (filepath, dependency, options) {
 	options = options || {};
-	options.types = options.types || DEFAULT_TYPES;
-	options.sources = options.sources || [];
-	if (options.sources.indexOf('node_modules') == -1) options.sources.push('node_modules');
+	options.type = options.type || 'js';
+	options.extensions = union(DEFAULT[options.type]['extensions'], options.extensions || []);
+	options.sources = union(DEFAULT[options.type]['sources'], options.sources || []);
 	var leadingChar = dependency.charAt(0)
 		, depFilepath;
 
+	// Force relative css paths
+	if (options.type == 'css'
+		&& dependency.indexOf('/') == -1
+		&& leadingChar != '.') {
+			dependency = './' + dependency;
+			leadingChar = '.';
+	}
+
 	// Absolute
 	if (leadingChar == '/') {
-		depFilepath = checkFile(dependency, options.types);
+		depFilepath = checkFile(dependency, options);
 	// Relative
 	} else if (leadingChar == '.') {
-		depFilepath = checkFile(path.resolve(path.dirname(filepath), dependency), options.types);
-	// node_modules package
-	} else if (dependency.indexOf('/') == -1) {
-		depFilepath = checkPackage(path.resolve('node_modules', dependency));
+		depFilepath = checkFile(path.resolve(path.dirname(filepath), dependency), options);
+	// Additional source locations, including node_modules
+	} else {
+		depFilepath = checkSources(dependency, options);
 	}
 	return depFilepath;
 };
 
-function checkFile (filepath, types) {
+/**
+ * Check the location of 'filepath'
+ * @param {String} filepath
+ * @param {Object} options
+ * @returns {String}
+ */
+function checkFile (filepath, options) {
+	// Already have a file extension
 	if (path.extname(filepath).length) {
 		return existsSync(filepath) ? filepath : '';
 	} else {
-		var p;
-		for (var i = 0, n = types.length; i < n; i++) {
-			p = path.resolve(filepath + '.' + types[i]);
-			if (existsSync(p)) return p;
-			p = path.resolve(filepath + '/index.' + types[i]);
-			if (existsSync(p)) return p;
+		var extensions = options.extensions
+			, rpath;
+		// Loop through extensions and locate file
+		for (var i = 0, n = extensions.length; i < n; i++) {
+			rpath = path.resolve(filepath + '.' + extensions[i]);
+			if (existsSync(rpath)) return rpath;
+			// Try index file
+			rpath = path.resolve(filepath + '/index.' + extensions[i]);
+			if (existsSync(rpath)) return rpath;
 		}
 		return '';
 	}
 }
 
+/**
+ * Check the location of 'dependency' in 'options.sources'
+ * @param {String} dependency
+ * @param {Object} options
+ * @returns {String}
+ */
+function checkSources (dependency, options) {
+	var sources = options.sources
+		, rpath, testpath;
+	// Loop through sources and locate file
+	for (var i = 0, n = sources.length; i < n; i++) {
+		rpath = path.resolve(sources[i], dependency);
+		// Handle node_modules
+		if (dependency.indexOf('/') == -1) {
+			if (testpath = checkPackage(rpath)) return testpath;
+		}
+		if (testpath = checkFile(rpath, options)) return testpath;
+	}
+	return '';
+}
+
+/**
+ * Check the location of 'filepath' in a package directory
+ * @param {String} filepath
+ * @returns {String}
+ */
 function checkPackage (filepath) {
-	var json, main, p;
+	var json, main, rpath;
 	if (existsSync(filepath)) {
+		// Parse package.json for 'main' field
 		if (existsSync(json = path.resolve(filepath, 'package.json'))
 			&& (main = JSON.parse(fs.readFileSync(json)).main)) {
-				p = path.resolve(filepath, main);
-				if (existsSync(p)) return p;
+				rpath = path.resolve(filepath, main);
+				if (existsSync(rpath)) return rpath;
+		// Fallback to index.js
 		} else {
-			p = path.resolve(filepath, 'index.js');
-			if (existsSync(p)) return p;
+			rpath = path.resolve(filepath, 'index.js');
+			if (existsSync(rpath)) return rpath;
 		}
 	}
+	return '';
+}
+
+/**
+ * Get union of passed arrays
+ * @param {Array} arguments
+ * @returns {Array}
+ */
+function union () {
+	return Array.prototype.concat.apply(Array.prototype, arguments).filter(function(el, i, array) {
+		return array.indexOf(el) == i;
+	});
 }
--- test/resolver-test.js
+++ test/resolver-test.js
@@ -10,25 +10,34 @@ describe('dependency-resolver', function() {
 	it('should resolve an absolute path', function() {
 		resolver('', path.resolve('foo.js')).should.eql(path.resolve('foo.js'));
 	});
-	it('should resolve a relative path to a file in the same directory', function() {
+	it('should resolve a relative path to a js file in the same directory', function() {
 		resolver(path.resolve('foo.js'), './baz').should.eql(path.resolve('baz.js'));
 	});
-	it('should resolve a relative path to a file in a child directory', function() {
+	it('should resolve a relative path to a js file in a child directory', function() {
 		resolver(path.resolve('foo.js'), './nested/foo').should.eql(path.resolve('nested/foo.js'));
 	});
-	it('should resolve a relative path to a file in a parent directory', function() {
+	it('should resolve a relative path to a js file in a parent directory', function() {
 		resolver(path.resolve('nested/foo.js'), '../baz').should.eql(path.resolve('baz.js'));
 	});
-	it('should not resolve a file with an unkown extension', function() {
+	it('should not resolve a js file with an unkown extension', function() {
 		resolver(path.resolve('foo.js'), './bar').should.not.be.ok;
 	});
-	it('should resolve a file with an unkown extension if optionally specified', function() {
-		resolver(path.resolve('foo.js'), './bar', {types: ['js', 'coffee', 'css']}).should.eql(path.resolve('bar.coffee'));
+	it('should resolve a js file with an unkown extension when optionally specified', function() {
+		resolver(path.resolve('foo.js'), './bar', {extensions: ['coffee']}).should.eql(path.resolve('bar.coffee'));
 	});
-	it('should resolve a node_module path containing a package.json file and a "main" field', function() {
+	it('should resolve a js node_module path containing a package.json file and a "main" field', function() {
 		resolver('', 'foo').should.eql(path.resolve('node_modules/foo/foo.js'));
 	});
-	it('should resolve a node_module path with no package.json file', function() {
+	it('should resolve a js node_module path with no package.json file', function() {
 		resolver('', 'bar').should.eql(path.resolve('node_modules/bar/index.js'));
+	});
+	it('should resolve a js file in a separate source directory when optionally specified', function() {
+		resolver('', 'package/foo', {sources: ['src']}).should.eql(path.resolve('src/package/foo.js'));
+	});
+	it('should resolve a css file in the same source directory', function() {
+		resolver(path.resolve('bar.css'), 'foo', {type: 'css'}).should.eql(path.resolve('foo.css'));
+	});
+	it('should resolve a css file in a separate source directory when optionally specified', function() {
+		resolver('', 'package/foo', {type: 'css', sources: ['src']}).should.eql(path.resolve('src/package/foo.css'));
 	});
 });
